67fc61e2c44e1a48a6525e29f1e83a625be9cc3b
IzviekovAndrii <67276774+IzviekovAndrii@users.noreply.github.com>
2020-09-14 14:11:24 +0300

    Update Example10_11_reverse_pair.py
    change comments

--- Chapter 10/Example10_11_reverse_pair.py
+++ Chapter 10/Example10_11_reverse_pair.py
@@ -1,4 +1,4 @@
-import Example10_10_in_bisect # make_list
+import Example10_10_in_bisect 
 
 def reverse(word):
 	return word[::-1]
@@ -7,20 +7,17 @@ def reverse_pair(t):
 	'''Two words are a “reverse pair” if each is the reverse of the other. 
 	Write a program	that finds all the reverse pairs in the word list'''
 	
-	for word in t:
-		if reverse(word) in t:
-			print(word, reverse(word))
-			# работает, но медленнно при большом списке
 	'''for word in t:
+		if reverse(word) in t:
+			print(word, reverse(word))'''
+			# работает, но медленнно при большом списке
+	for word in t:
 		if Example10_10_in_bisect.in_bisect(t, reverse(word)):
-			print(word, reverse(word)) '''  
-			#не работает из-за ошибки предыдущей фунции
+			print(word, reverse(word))  
 
 
 if __name__ == '__main__':	
 	s = 'words.txt'
 	t = Example10_10_in_bisect.make_list(s)
 	small_t = Example10_10_in_bisect.make_list(s)[::10] # make list whith lenth 10 time smaller original
-	#print(small_t)
 	reverse_pair(small_t)
-	#reverse_pair(t)
